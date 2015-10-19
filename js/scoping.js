cd5047c5ca92f9cb4cffea871cf07c601a3ebed5
Dave Stearns <dlsinfo@uw.edu>
2015-10-19 14:42:57 -0700

    file stubs for scoping exercise

---
+++ js/scoping.js
@@ -0,0 +1,28 @@
+/*
+    script file for the scoping.html page
+
+    If you struggled understanding variable scoping during the last challenge,
+    do this simple exercise. You goal is to show the number of times the
+    user clicks the "Click Me" button. Write the code necessary to accomplish
+    the following:
+
+    - Use jQuery to register a function that will be called once the DOM
+    is ready for manipulation.
+    https://learn.jquery.com/using-jquery-core/document-ready/
+
+    - Remember to add the 'use strict'; directive inside that function to switch
+    into strict mode.
+    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Strict_mode
+
+    - When the DOM is ready, use jQuery to register a function that will be
+    called whenever the user clicks on the "Click Me" button.
+    http://api.jquery.com/click/
+
+    - Declare a variable at the proper scope to record how many times the user
+    clicked on that button.
+
+    - When the user clicks the button, increment your variable and display
+    the current count in the <span id="clicked-count"> element.
+    http://api.jquery.com/text/
+*/
+
